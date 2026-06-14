3f206db7da31dc2a7462d98836570b0af3eac75e
rawandjoacdemy <rawand.abuhasneh@joacademy.com>
2026-06-14 18:16:28 +0300

    create project, create main functions add, remove, working on done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,62 @@
-import Image from "next/image";
+"use client";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [tasksList, setTasksList] = useState([
+    { text: "", id: Date.now(), isDone: false },
+  ]);
+  const inputRef = useRef("");
+  function addTask(task: string) {
+    const taskItem = {
+      id: Date.now(),
+      text: task,
+      isDone: false,
+    };
+
+    setTasksList([...tasksList, taskItem]);
+  }
+
+  function removeTask(id) {
+    const taskItems = tasksList.filter((task) => task.id !== id);
+    setTasksList(taskItems);
+  }
+
+  function handleCheckbox(task) {
+    task.isDone = true;
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      <h1>ToDo List</h1>
+
+      <h2>To Do</h2>
+      <div>
+        <input ref={inputRef} type="text" placeholder="task name" />
+        <button onClick={() => addTask(inputRef.current.value)}>add</button>
+      </div>
+      {console.log(inputRef.current.value)}
+      <div>
+        <ul>
+          {tasksList.map((task, idx) => {
+            return (
+              <li key={idx}>
+                <input
+                  type="checkbox"
+                  onChange={handleCheckbox}
+                  checked={isChecked}
+                />{" "}
+                {task.text}{" "}
+                <button onClick={() => removeTask(task.id)}>delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <h2>Done</h2>
+      <div>
+        <ul>donelist.map</ul>
+      </div>
+    </>
   );
 }
---
+++ src/store/tasksStore.tsx
@@ -0,0 +1,9 @@
+// import { create } from "zustand";
+
+
+// export default function useTasksStore(){
+//     create((set)=>({
+//         tasksList: [],
+//         setTasksList: () => set((state)=> ({tasksList: state.tasksList}) )
+//     }))
+// }
